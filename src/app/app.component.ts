182b6f7d4e32910fd6e9c54544c2d2522fc8587e
chhedadhaval120386 <chhedadhaval120386@gmail.com>
2020-07-23 07:26:59 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'co-angular';
+
+  constructor(private http: HttpClient) {}
+
+  addOrganisation(addForm: NgForm) {
+    this.http
+      .post('http://localhost:8000/api/organisations', addForm.value)
+      .subscribe((data) => console.log('data', data));
+  }
+
+  searchOrganisation(searchForm: NgForm) {
+    const name = searchForm.value.name;
+    this.http
+      .get(`http://localhost:8000/api/organisations/${name}`)
+      .subscribe((data) => {
+        console.log('data', data);
+      });
+  }
 }
